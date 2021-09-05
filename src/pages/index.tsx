3de9a484a36973d656ecfd98098ba402a4dc1ce2
raphael <raphael.fernandes@intelie.com.br>
2021-09-05 18:25:00 -0300

    To send

---
+++ src/pages/index.tsx
@@ -0,0 +1,80 @@
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton'
+import {stripe} from '../services/stripe'
+
+interface HomeProps {
+  
+  product:{
+    
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product} ) {
+
+  
+  
+  return (
+    <>
+    <Head>
+    <title>Home | news</title>
+    </Head>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications
+            <span>for {product.amount} month</span>
+          </p>
+        <SubscribeButton/>
+      </section>
+      
+      <img src="/images/avatar.svg" alt="Girl Coding"/>
+    </main>
+    </>
+  )
+}
+
+
+export const getServerSideProps:GetServerSideProps = async ()=>{
+  const price = await stripe.prices.retrieve(
+    'price_1JW8lKCUQz1UtjErDKtLoYJu',
+    {
+    expand: ['product']
+    }
+  )
+  
+  const product ={
+    
+    priceId: price.id,
+    amount: price.unit_amount/100,
+  }
+  
+  return {
+    
+    props: {
+     product
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
