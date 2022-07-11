55575d78e2e1b36df39324e1e78f7132891678b8
Raphael Fernandes <raphael.fernandes.eng@gmail.com>
2022-07-11 19:19:51 -0300

    Fetching posts and showing it on the page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,80 @@
+import styles from './styles.module.scss'
+import {  } from 'next/head';
+import { GetStaticProps } from 'next';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from "@prismicio/client"
+import {RichText} from 'prismic-dom'
+
+type Post = {
+          slug: string;
+          title: string;
+          excerpt: string;
+          updatedAt: string;
+}
+
+interface PostsProps {
+          
+          posts: Post[]
+}
+
+export default function Posts({posts}: PostsProps) {
+          
+return(
+<>
+<head>
+<title>Post | news</title>
+</head>
+
+<main className={styles.container}>
+          <div className={styles.posts}>
+          {posts.map(post => (
+                    <a key={post.slug} href="#">
+                              <time>{post.updatedAt}</time>
+                              <strong>{post.title}</strong>
+                              <p>{post.excerpt}</p>
+                              
+                    </a>
+          ))}
+          </div>
+</main>
+</>
+);
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+          
+          const prismic = getPrismicClient ();
+          
+          const response = await prismic.query([
+
+                    Prismic.predicates.at('document.type', 'publication')
+          ], {
+                    fetch: ['publication.title', 'publication.content'],
+                    pageSize: 100,
+          })
+          
+          // console.log(JSON.stringify(response, null, 2), "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+          
+          const posts = response.results.map(post =>{
+          return{
+          slug: post.uid,
+          // title: RichText.asText(post.data.title),
+          title: post.data.title,
+          excerpt: post.data.content[0].text.find(content => content.type ==='paragraph')?.text ?? '',
+          // excerpt: post.data.content.find(content => content.type ==='paragraph')?.text ?? '',
+          updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                    })
+          }
+          })
+          console.log(posts, "PPPPPPPPPPPPPPPPPPPPPPPP")
+          
+          return {
+                    
+                    props: {
+                              posts
+                    }
+          }
+}
